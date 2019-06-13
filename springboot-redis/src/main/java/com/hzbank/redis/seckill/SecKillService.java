95104286f58c7248d8ad58b27e5abf73da5b9934
lihao <543627194@qq.com>
2019-06-13 12:26:22 +0800

    秒杀demo，并不完美

--- springboot-redis/src/main/java/com/hzbank/redis/seckill/SecKillService.java
+++ springboot-redis/src/main/java/com/hzbank/redis/seckill/SecKillService.java
@@ -3,19 +3,29 @@ package com.hzbank.redis.seckill;
 import com.hzbank.redis.entity.Product;
 import com.hzbank.redis.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
 public class SecKillService {
 
+    private static final String lockKey = "seckill";
+
     @Autowired
     private RedisTemplate redisTemplate;
 
     @Autowired
+    private RedissonClient redissonClient;
+
+    @Autowired
     private ProductService productService;
+
 
     /**
      * 秒杀前一分钟读取库存到redis的zset集合中
@@ -39,18 +49,32 @@ public class SecKillService {
      */
     public void sendMsg(Integer productId,Integer userId) {
 
+
         //接受用户请求，从redis查询库存，判断是否放入redis的list队列中
         Double count = redisTemplate.opsForZSet().score("seckill", productId);
         if (count <= 0) {
-            log.info("Sorry，活动太火爆了，被抢完了，稍等有用户未付款，您还有机会。"+userId);
+            log.info("1 Sorry，活动太火爆了，被抢完了，稍等有用户未付款，您还有机会。"+userId);
             return;
         }
         //如果用户请求数大于商品数量，返回抢购失败
         Long  size= redisTemplate.opsForList().size(productId);
         if (size > count) {
-            log.info("Sorry，活动太火爆了，被抢完了，稍等有用户未付款，您还有机会。"+userId);
+            log.info("2 Sorry，活动太火爆了，被抢完了，稍等有用户未付款，您还有机会。"+userId);
             return;
         }
+
+//        RLock lock = redissonClient.getLock(lockKey);
+//        lock.lock(10,TimeUnit.SECONDS);
+//        if (!Thread.currentThread().isInterrupted()) {
+//
+//            try {
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            } finally {
+//                lock.unlock();
+//            }
+//        }
         //队列用rabbitMQ更好
         redisTemplate.opsForList().leftPush(productId, userId);
         log.info(userId+" 进入队列");
@@ -70,17 +94,17 @@ public class SecKillService {
                 Integer userId1 = (Integer) redisTemplate.opsForList().rightPop(productId);
                 Product product = productService.findOne(productId);
                 if (product.getCount() <= 0) {
-                    log.info( "Sorry，活动太火爆了，被抢完了，稍等有用户未付款，您还有机会。"+userId1);
+                    log.info( "3 Sorry，活动太火爆了，被抢完了，稍等有用户未付款，您还有机会。"+userId1);
                     return;
                 }
 
                 //锁定库存，同时修改redis的库存数量，返回抢购成功
                 productService.decreCount(productId);//数据库库存递减
                 redisTemplate.opsForZSet().incrementScore("seckill", productId, -1);
+                log.info("缓存中库存数量-->"+redisTemplate.opsForZSet().score("seckill", productId)+" 数据库中库存数量-->"+(product.getCount()-1));
                 log.info("抢购成功...... "+userId1);
             }
         }
-
-
     }
+
 }
--- springboot-redis/src/test/java/com/hzbank/redis/test/TestSecKill.java
+++ springboot-redis/src/test/java/com/hzbank/redis/test/TestSecKill.java
@@ -8,9 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.CountDownLatch;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringbootRedis.class)
 public class TestSecKill {
+
+    private static final int THREAMCOUNT=1000;
+    //倒计数，用于并发测试
+    private CountDownLatch latch = new CountDownLatch(THREAMCOUNT);
 
     @Autowired
     private SecKillService secKillService;
@@ -21,10 +27,38 @@ public class TestSecKill {
     }
 
     @Test
-    public void secKill(){
-        for (int i = 0; i < 200; i++) {
-            secKillService.sendMsg(2,i);
+    public void secKill() throws InterruptedException {
+
+        for (int i = 0; i <THREAMCOUNT ; i++) {
+            new Thread(new Send(i,2)).start();
+            latch.countDown();//等待线程减1
+        }
+        //主线程挂起
+        Thread.currentThread().join();
+    }
+
+
+    class Send implements Runnable {
+
+        private int id;
+        private int productId;
+
+        public Send(int id, int productId) {
+            this.id = id;
+            this.productId = productId;
         }
 
+        @Override
+        public void run() {
+            try {
+                //所有子线程在这里等待，当所有子线程实例化后，同时停止等待
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            secKillService.sendMsg(2,id);
+        }
     }
 }
+
+
